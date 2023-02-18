3272d5cfb922f5800893baf41e8efe1883835833
Vijendra Singh Chouhan <48381995+vijendra2010@users.noreply.github.com>
2023-02-18 17:46:27 +0530

    Update and rename SQLAssignment1.md to SQLAssignment1.sql

---
+++ SQLAssignment1.sql
@@ -0,0 +1,10 @@
+Creating the table CITY as per the given schema.
+
+	create table city
+(
+    id INT,
+    name VARCHAR(17),
+    countrycode VARCHAR(6),
+    district VARCHAR(20),
+    population INT
+);
