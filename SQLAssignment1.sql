d57d76e06da5bc0349c628862d4f1424f14d693e
Vijendra Singh Chouhan <48381995+vijendra2010@users.noreply.github.com>
2023-02-27 23:31:36 +0530

    Update SQLAssignment1.sql

--- SQLAssignment1.sql
+++ SQLAssignment1.sql
@@ -136,3 +136,42 @@ AND SUBSTR(city, LENGTH(city)) NOT IN ('a', 'e', 'i', 'o', 'u');
 SELECT DISTINCT(city) FROM station WHERE city NOT REGEXP '^[aeiou]' AND city NOT REGEXP '[aeuoi]$';
 
 
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+--Q17. Creating product table and sales table and entering values in them.
+
+CREATE TABLE product
+(
+    product_id INT,
+    product_name VARCHAR(20),
+    unit_price INT,
+    CONSTRAINT pk PRIMARY KEY (product_id)
+);
+
+CREATE TABLE sales
+(
+    seller_id INT,
+    product_id INT,
+    buyer_id INT,
+    sale_date DATE,
+    quantity INT,
+    price INT,
+    CONSTRAINT fk FOREIGN KEY (product_id) REFERENCES product(product_id)
+);
+
+INSERT INTO product VALUES(1, "S8", 1000), (2, "G4", 800), (3, "iPhone", 1400); 
+SELECT * FROM product;
+
+INSERT INTO sales VALUES(1, 1, 1, '2019-01-21', 2, 2000), (1, 2, 2, '2019-02-17', 1, 800),(2, 2, 3, '2019-06-02', 1, 800),
+(3, 3, 4, '2019-05-13', 2, 2800);
+SELECT * FROM sales;
+
+
+--Write an SQL query that reports the products that were only sold in the first quarter of 2019. That is,between 2019-01-01 and 2019-03-31 inclusive.
+--Return the result table in any order.
+
+
