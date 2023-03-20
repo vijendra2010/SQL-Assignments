5c76763b860be3e4bad0641118fb737002e50b39
Vijendra Singh Chouhan <48381995+vijendra2010@users.noreply.github.com>
2023-03-20 22:40:42 +0530

    Update SQLAssignment1.sql

--- SQLAssignment1.sql
+++ SQLAssignment1.sql
@@ -355,6 +355,42 @@ GROUP BY country_name;
 
 
 
+--Q23. Write an SQL query to find the average selling price for each product. average_price should be rounded to 2 decimal places.
+--Return the result table in any order. The query result format is in the following example.
+
+--Create table Prices
+CREATE TABLE Prices (
+    product_id INT,
+    start_date DATE,
+    end_date DATE,
+    price INT,
+    CONSTRAINT pk PRIMARY KEY (product_id, start_date, end_date)
+);
+
+--Insert record into table Prices
+INSERT INTO Prices VALUES
+(1, "2019-02-17", "2019-02-28", 5),
+(1, "2019-03-01", "2019-03-22", 20),
+(2, "2019-02-01", "2019-02-20", 15),
+(2, "2019-02-21", "2019-03-31", 30);
+
+--Create table UnitsSold
+CREATE TABLE UnitsSold (
+    product_id INT,
+    purchase_date DATE,
+    units INT 
+);
+
+--Insert records into UnitsSold
+INSERT INTO UnitsSold VALUES
+(1, "2019-02-25", 100),
+(1, "2019-03-01", 15),
+(2, "2019-02-10", 200),
+(2, "2019-03-22", 30);
+
+
+
+
 
 
 
