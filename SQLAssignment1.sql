7126e34f7a1ee43e57c1fe69abbb558567409c21
Vijendra Singh Chouhan <48381995+vijendra2010@users.noreply.github.com>
2023-03-20 22:04:58 +0530

    Update SQLAssignment1.sql

--- SQLAssignment1.sql
+++ SQLAssignment1.sql
@@ -288,6 +288,74 @@ ORDER BY ctr DESC, ad_id ASC;
 
 
 
+--Q22. Write an SQL query to find the type of weather in each country for November 2019.
+--The type of weather is:
+--● Cold if the average weather_state is less than or equal 15,
+--● Hot if the average weather_state is greater than or equal to 25, and
+--● Warm otherwise.
+--Return result table in any order.
+--The query result format is in the following example.
+
+--Create table Countries
+CREATE TABLE Countries (
+    country_id INT,
+    country_name VARCHAR(30),
+    CONSTRAINT pk PRIMARY KEY (country_id)
+);
+
+--Insert records in table Countries
+INSERT INTO Countries VALUES 
+(2, "USA"),
+(3, "Australia"),
+(7, "Peru"),
+(5, "China"),
+(8, "Morocco"),
+(9, "Spain");
+
+SELECT * FROM Countries;
+
+--Create table Weather
+CREATE TABLE Weather (
+    country_id INT,
+    weather_state INT,
+    day DATE,
+    CONSTRAINT pk PRIMARY KEY (country_id, day)
+);
+
+--Insert records into table Weather
+INSERT INTO Weather VALUES
+(2,15,'2019-11-01'),(2,12,'2019-10-28'),
+(2,12,'2019-10-27'),(3,-2,'2019-11-10'),
+(3,0,'2019-11-11'),(3,3,'2019-11-12'),
+(5,16,'2019-11-07'),(5,18,'2019-11-09'),
+(5,21,'2019-11-23'),(7,25,'2019-11-28'),
+(7,22,'2019-12-01'),(7,20,'2019-12-02'),
+(8,25,'2019-11-05'),(8,27,'2019-11-15'),
+(8,31,'2019-11-25'),(9,7,'2019-10-23'),
+(9,3,'2019-12-23');
+
+SELECT * FROM Weather ORDER BY country_id ASC;
+
+--Solution
+--First JOIN the two tables as we need columns from both the tables in the end result.
+--Filter out the rows for november month.
+--Group by the records country name wise and select the required columns.
+
+SELECT c.country_name, AVG(w.weather_state), 
+CASE
+    WHEN AVG(w.weather_state) <= 15 THEN 'cold'
+    WHEN AVG(w.weather_state) <= 25 THEN 'hot'
+    ELSE 'warm'
+END AS status_report    
+FROM Countries c INNER JOIN Weather w
+ON c.country_id = w.country_id
+WHERE w.day BETWEEN '2019-11-01' AND '2019-11-30'
+GROUP BY country_name;
+
+
+
+
+
 
 
 
